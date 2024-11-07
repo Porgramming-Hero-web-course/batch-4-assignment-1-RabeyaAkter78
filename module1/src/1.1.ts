d41c035824f580c19ded76f9b758993396a262c3
Rabeya Akter <rabeyariya251@gmail.com>
2024-11-07 19:45:57 +0600

    first commit

---
+++ module1/src/1.1.ts
@@ -0,0 +1,9 @@
+
+const sumOfArray = (array: number[]): number => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum;
+};
+console.log(sumOfArray([1, 2, 3, 4, 5]))
